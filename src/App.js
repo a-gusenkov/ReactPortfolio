8fc678336c39820fb68d67e5418fda228007c145
a-gusenkov <anastasiagusenkov1@gmail.com>
2021-12-20 19:36:46 -0500

    push

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logoB from '/Users/stasia.gus/Desktop/webport/src/MYLOGO.png';
+import React from 'react';
+import DemoPortfolio from './demoPortfolio';
+
+
+function App() {
+  return (
+    <>
+     <DemoPortfolio/>
+    </>
+  );
+}
+
+export default App;
